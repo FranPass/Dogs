52425b08dc35cc6f7a6db69b1399e08d30f07d5e
FPassetti <franciscopassettimail.com>
2023-06-20 20:01:36 -0300

    g

--- client_vite/src/components/LandingPage/LandingPage.jsx
+++ client_vite/src/components/LandingPage/LandingPage.jsx
@@ -2,11 +2,10 @@ import { NavLink } from "react-router-dom";
 import style from "./LandingPage.module.css";
 
 
-
 export default function LandingPage() {
 
     return (
-        <div className={style.containerLanding}>
+        <div className={style.container}>
             <div className={style.landingpage}>
                 <img
                     src="https://imagenpng.com/wp-content/uploads/2016/09/Grupo-perros.png"
