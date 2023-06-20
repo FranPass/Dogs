6e12123b347f3991dc6f499aa657a0f1051dcbb6
FPassetti <franciscopassettimail.com>
2023-06-20 19:01:22 -0300

    g

--- client_vite/src/components/LandingPage/LandingPage.jsx
+++ client_vite/src/components/LandingPage/LandingPage.jsx
@@ -1,16 +1,16 @@
 import { NavLink } from "react-router-dom";
-import style from "./LandingPage.module.css";
+// import style from "./LandingPage.module.css";
 
 export default function LandingPage() {
 
     return (
-        <div className={style.container}>
-            <div className={style.landingpage}>
+        <div>
+            <div>
                 <img
                     src="https://imagenpng.com/wp-content/uploads/2016/09/Grupo-perros.png"
                     alt=""
                     />
-                <h1>HENRY’s DOGS WORLD</h1>
+                <h1>HENRYs DOGS WORLD</h1>
                 <NavLink to="/home">
                     <button>Start!</button>
                 </NavLink>
