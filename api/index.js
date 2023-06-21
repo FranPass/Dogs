8bde4cd7bf0aff68851edd19748c2db0c7b9e8d0
FPassetti <franciscopassettimail.com>
2023-06-20 23:25:29 -0300

    default url server

--- api/index.js
+++ api/index.js
@@ -19,10 +19,9 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-console.log('Loading server holis');
 
 conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('%s listening at 3001'); 
   });
 });
