d9795a8a20596702473d18e6e38cd1de72acff6e
FPassetti <franciscopassettimail.com>
2023-06-20 21:34:03 -0300

    serverr

--- api/index.js
+++ api/index.js
@@ -19,8 +19,8 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+console.log('Loading server holis');
 
-// Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
